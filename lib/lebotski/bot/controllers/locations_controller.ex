843cc2e74ed12ee59b2cde27c915f9271e7c95f1
Jamie Wright <jamie@brilliantfantastic.com>
2022-08-23 11:10:43 -0400

    Add the correct parameter and return the context

--- lib/lebotski/bot/controllers/locations_controller.ex
+++ lib/lebotski/bot/controllers/locations_controller.ex
@@ -1,7 +1,10 @@
 defmodule Lebotski.Bot.Controllers.LocationsController do
   use Juvet.Controller
 
-  def pharmacies do
+  def pharmacies(%{request: %{params: params}} = context) do
+    IO.inspect(params)
     IO.puts("You called?")
+
+    {:ok, context}
   end
 end
