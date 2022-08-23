1cc9b1f8d3301f33be34dce3778d860116d968cb
Jamie Wright <jamie@brilliantfantastic.com>
2022-08-23 16:30:26 -0400

    Add test factory support

--- mix.exs
+++ mix.exs
@@ -49,6 +49,8 @@ defmodule Lebotski.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
+      {:ex_machina, "~> 2.7", only: :test},
+      {:faker, "~> 0.17", only: :test},
       {:juvet, git: "https://github.com/juvet/juvet.git", branch: "main"},
       {:yelp_ex, "~> 0.2.0"}
     ]
