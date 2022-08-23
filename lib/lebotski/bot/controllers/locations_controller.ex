18f13759d5d977042af8e66a07e8ca4b33e62cf7
Jamie Wright <jamie@brilliantfantastic.com>
2022-08-23 17:47:09 -0400

    Create a new team when the locations endpoint is called

--- lib/lebotski/bot/controllers/locations_controller.ex
+++ lib/lebotski/bot/controllers/locations_controller.ex
@@ -1,17 +1,17 @@
 defmodule Lebotski.Bot.Controllers.LocationsController do
   use Juvet.Controller
 
-  alias Juvet.Router.Response
-  alias Lebotski.Users
+  alias Lebotski.{Teams, Users}
 
   def pharmacies(%{request: %{params: params, platform: platform}} = context) do
     {:ok, _user} = Users.create_user(%{external_id: params["user_id"], platform: platform})
+    {:ok, _team} = Teams.create_team(%{external_id: params["team_id"], platform: platform})
 
     # TODO: Check for an address
     # If no address, check for a last one in the database
     # If there are none, send an error back
     # else send back calculating...
-    context = send_response(context, Response.new(body: %{text: "Gotcha!"}))
+    context = send_response(context, Juvet.Router.Response.new(body: %{text: "Gotcha!"}))
 
     # Geocode the address
     # Use the weedmaps API to find based on lat/long
--- test/lebotski/bot/controllers/locations_controller_test.exs
+++ test/lebotski/bot/controllers/locations_controller_test.exs
@@ -1,13 +1,16 @@
 defmodule Lebotski.Bot.Controllers.LocationsControllerTest do
   use LebotskiWeb.ConnCase, async: true
 
-  alias Lebotski.{Repo, Users}
+  alias Lebotski.{Repo, Teams, Users}
   alias Lebotski.Bot.Controllers.LocationsController
+
+  def user_count, do: Users.count_users()
+  def team_count, do: Teams.count_teams()
 
   describe "pharmacies/1 with a valid request" do
     setup %{conn: conn} do
       params = %{
-        "channel_id" => "C12345",
+        "team_id" => "T12345",
         "text" => "West Bromwich",
         "user_id" => "U12345",
         "username" => "robert"
@@ -25,13 +28,10 @@ defmodule Lebotski.Bot.Controllers.LocationsControllerTest do
     end
 
     test "creates a user", %{context: context} do
-      user_count = Users.count_users()
+      previous_user_count = user_count()
 
       assert {:ok, _context} = LocationsController.pharmacies(context)
-
-      new_user_count = Users.count_users()
-
-      assert user_count + 1 == new_user_count
+      assert previous_user_count + 1 == user_count()
 
       user =
         Users.User
@@ -40,6 +40,21 @@ defmodule Lebotski.Bot.Controllers.LocationsControllerTest do
 
       assert user.platform == :slack
       assert user.external_id == "U12345"
+    end
+
+    test "creates a team", %{context: context} do
+      previous_team_count = team_count()
+
+      assert {:ok, _context} = LocationsController.pharmacies(context)
+      assert previous_team_count + 1 == team_count()
+
+      team =
+        Teams.Team
+        |> Ecto.Query.last()
+        |> Repo.one()
+
+      assert team.platform == :slack
+      assert team.external_id == "T12345"
     end
   end
 end
