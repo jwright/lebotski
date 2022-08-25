bb569d4e829a0efc89e872585e1ab98ea92e7fc6
Jamie Wright <jamie@brilliantfantastic.com>
2022-08-25 15:56:36 -0400

    First action for getting more information

---
+++ lib/lebotski/bot/actions.ex
@@ -0,0 +1,1 @@
+alias Lebotski.Bot.Actions.Locations
---
+++ lib/lebotski/bot/actions/locations.ex
@@ -0,0 +1,10 @@
+defmodule Lebotski.Bot.Actions.Locations do
+  @moduledoc false
+
+  @namespace "locations"
+
+  def more_info, do: action("more_info")
+
+  defp action(action), do: "#{namespace}.#{action}"
+  defp namespace, do: @namespace
+end
