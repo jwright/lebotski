68f8a7b3980db1e55bac4bc55aeec3470138a24a
Jamie Wright <jamie@brilliantfantastic.com>
2022-08-27 12:55:39 -0400

    Add the command for the bowling alleys

--- lib/lebotski/bot/router.ex
+++ lib/lebotski/bot/router.ex
@@ -4,6 +4,10 @@ defmodule Lebotski.Bot.Router do
   alias Lebotski.Bot.Commands
 
   platform :slack do
+    command(Commands.Locations.bowling_alleys(),
+      to: "lebotski.bot.controllers.locations#bowling_alleys"
+    )
+
     command(Commands.Locations.pharmacies(), to: "lebotski.bot.controllers.locations#pharmacies")
   end
 end
