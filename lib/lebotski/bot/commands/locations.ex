2c3433aa54dde16d3c9597e405a584f170a78cc9
Jamie Wright <jamie@brilliantfantastic.com>
2022-08-27 13:07:50 -0400

    Add the cocktail bar command

--- lib/lebotski/bot/commands/locations.ex
+++ lib/lebotski/bot/commands/locations.ex
@@ -2,6 +2,7 @@ defmodule Lebotski.Bot.Commands.Locations do
   @moduledoc false
 
   def bowling_alley, do: command("not-nam")
+  def cocktail_bar, do: command("beverage-here-man")
   def pharmacy, do: command("what-day-is-this")
 
   defp command(command), do: "/#{command}"
