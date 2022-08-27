190ac6d45adffa15789c18d6d84ee206bf094026
Jamie Wright <jamie@brilliantfantastic.com>
2022-08-27 13:59:09 -0400

    Update the images for the cocktail bar and bowling search results

--- lib/lebotski/bot/templates/search_results_category_image.ex
+++ lib/lebotski/bot/templates/search_results_category_image.ex
@@ -4,9 +4,8 @@ defmodule Lebotski.Bot.Templates.SearchResultsCategoryImage do
   def to_message(%Category{term: :bowling_alley}) do
     %{
       type: "image",
-      image_url:
-        "https://dangerousminds.net/content/uploads/images/made/content/uploads/images/JeffBridges_TBLsdfsdfsdf_465_291_int.jpg",
-      alt_text: "Doo, doo, doo, looking out that backdoor."
+      image_url: "https://i.ytimg.com/vi/dQctpzuXmUs/maxresdefault.jpg",
+      alt_text: "This is not 'Nam. This is bowling. There are rules.'"
     }
   end
 
@@ -14,8 +13,8 @@ defmodule Lebotski.Bot.Templates.SearchResultsCategoryImage do
     %{
       type: "image",
       image_url:
-        "https://dangerousminds.net/content/uploads/images/made/content/uploads/images/JeffBridges_TBLsdfsdfsdf_465_291_int.jpg",
-      alt_text: "Doo, doo, doo, looking out that backdoor."
+        "https://focusmicrositesprod.s3.amazonaws.com/assets/uploads/post_marquee_5f19f76b2870d.jpg",
+      alt_text: "Hey, careful man. There's a beverage here!"
     }
   end
 
