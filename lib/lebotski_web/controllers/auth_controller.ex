653ae87bbab8677b15b81fe8f7792777a004b999
Jamie Wright <jamie@brilliantfantastic.com>
2022-08-28 17:13:52 -0400

    Initial implementation of the callback phase

--- lib/lebotski_web/controllers/auth_controller.ex
+++ lib/lebotski_web/controllers/auth_controller.ex
@@ -3,9 +3,19 @@ defmodule LebotskiWeb.AuthController do
 
   alias LebotskiWeb.OAuth
 
+  def callback(conn, %{"provider" => provider, "code" => code}) do
+    %{token: token} = get_token!(provider, code)
+
+    token |> IO.inspect(label: "token")
+
+    render(conn, "success.html")
+  end
+
   def request(conn, %{"provider" => provider}),
     do: redirect(conn, external: authorize_url!(provider))
 
   defp authorize_url!("slack"),
     do: OAuth.Slack.authorize_url!(scope: ["commands"] |> Enum.join(","))
+
+  defp get_token!("slack", code), do: OAuth.Slack.get_token!(code: code)
 end
