35a5c4ab9ce0cc12a771cf242f8e599286a7e61d
Jamie Wright <jamie@brilliantfantastic.com>
2022-08-28 13:37:22 -0400

    Add to Slack button

--- lib/lebotski_web/controllers/page_controller.ex
+++ lib/lebotski_web/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule LebotskiWeb.PageController do
   use LebotskiWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html",
+      slack_client_id: System.get_env("SLACK_CLIENT_ID"),
+      slack_scopes: ["commands"] |> Enum.join(",")
+    )
   end
 end
